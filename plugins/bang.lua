cc0189414b4a55a471022b62415c8495c70067d9
mrbangi <xboxlivemnasr@gmail.com>
2016-01-25 19:50:46 +0330

    Update bang.lua

--- plugins/bang.lua
+++ plugins/bang.lua
@@ -1,10 +1,8 @@
   do
-  function send.msg('user#id')
-  if msg.text == "send" then
+  if msg.text ==("send")then
   return ('user#id')
-  end
+usage = "!send send
+end
    end
 	run = run,
-    --privileged = true,
-	pre_process = pre_process
 }
