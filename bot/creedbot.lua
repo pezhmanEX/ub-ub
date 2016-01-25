0f33b2796410a315106aa16032ab771fe8f12049
mrbangi <xboxlivemnasr@gmail.com>
2016-01-25 19:00:07 +0330

    Update creedbot.lua

--- bot/creedbot.lua
+++ bot/creedbot.lua
@@ -252,38 +252,55 @@ function create_config( )
 Realm Commands:
 !creategroup [Name]
 Create a group
+
 !createrealm [Name]
 Create a realm
+
 !setname [Name]
 Set realm name
+
 !setabout [GroupID] [Text]
 Set a group's about text
+
 !setrules [GroupID] [Text]
 Set a group's rules
+
 !lock [GroupID] [setting]
 Lock a group's setting
+
 !unlock [GroupID] [setting]
 Unock a group's setting
+
 !wholist
 Get a list of members in group/realm
+
 !who
 Get a file of members in group/realm
+
 !type
 Get group type
+
 !kill chat [GroupID]
 Kick all memebers and delete group
+
 !kill realm [RealmID]
 Kick all members and delete realm
+
 !addadmin [id|username]
 Promote an admin by id OR username *Sudo only
+
 !removeadmin [id|username]
 Demote an admin by id OR username *Sudo only
+
 !list groups
 Get a list of all groups
+
 !list realms
 Get a list of all realms
+
 !log
 Grt a logfile of current group or realm
+
 !broadcast [text]
 !broadcast Hello !
 Send text to all groups
@@ -292,51 +309,76 @@ Send text to all groups
 Commands list :
 !kick [username|id]
 You can also do it by reply
+
 !ban [ username|id]
 You can also do it by reply
+
 !unban [id]
 You can also do it by reply
+
 !who
 Members list
+
 !modlist
 Moderators list
+
 !promote [username]
 Promote someone
+
 !demote [username]
 Demote someone
+
 !kickme
 Will kick user
+
 !about
 Group description
+
 !setphoto
 Set and locks group photo
+
 !setname [name]
 Set group name
+
 !rules
 Group rules
+
 !id
 return group id or user id
+
 !help
 !lock [member|name|bots|leave]	
+
 Locks [member|name|bots|leaveing] 
+
 !unlock [member|name|bots|leave]
+
 Unlocks [member|name|bots|leaving]
+
 !set rules <text>
 Set <text> as rules
+
 !set about <text>
 Set <text> as about
+
 !settings
 Returns group settings
+
 !newlink
 create/revoke your group link
+
 !link
 returns group link
+
 !owner
 returns group owner id
+
 !setowner [id]
 Will set id as owner
+
 !setflood [value]
 Set [value] as flood sensitivity
+
 !stats
 Simple message statistics
 !save [value] <text>
