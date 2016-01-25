3ff6f327aab27c1d9b7c107a7590bf0490db7224
mrbangi <xboxlivemnasr@gmail.com>
2016-01-25 20:32:54 +0330

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -1,3 +1,4 @@
+
 do
 
 -- Returns a table with `name` and `msgs`
@@ -72,7 +73,6 @@ local function bot_stats()
   local redis_scan = [[
     local cursor = '0'
     local count = 0
-
     repeat
       local r = redis.call("SCAN", cursor, "MATCH", KEYS[1])
       cursor = r[1]
@@ -91,10 +91,10 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'creedbot' then -- Put everything you like :)
+  if matches[1]:lower() == 'teleseed' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /creedbot ")
+    savelog(msg.to.id, name.." ["..msg.from.id.."] used /teleseed ")
     return about
   end 
   if matches[1]:lower() == "statslist" then
@@ -120,7 +120,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "creedbot" then -- Put everything you like :)
+    if matches[2] == "pokerface" then -- Put everything you like :)
       if not is_admin(msg) then
         return "For admins only !"
       else
@@ -141,8 +141,8 @@ return {
     "^[!/]([Ss]tats)$",
     "^[!/]([Ss]tatslist)$",
     "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (creedbot)",-- Put everything you like :)
-		"^[!/]([Cc]reedbot)"-- Put everything you like :)
+    "^[!/]([Ss]tats) (poker)",-- Put everything you like :)
+		"^[!/]([Pp]oker)"-- Put everything you like :)
     }, 
   run = run
 }
