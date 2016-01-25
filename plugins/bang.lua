9d76fe7ef2c6a4f98188168d2dcddf85a7b048d5
mrbangi <xboxlivemnasr@gmail.com>
2016-01-25 20:09:15 +0330

    Update bang.lua

--- plugins/bang.lua
+++ plugins/bang.lua
@@ -1,8 +1,34 @@
-  do
-  if msg.text ==("send")then
-  return ('user#id')
-usage = "!send send
+local function run(msg)
+if msg.text == "channel" then
+	return "@bangi_channel\n pokerface a private bot for @mrbangi_org"
 end
-   end
+local function run(msg)
+if msg.text == "bangi"
+	return "joonam"
+end
+local function run(msg)
+if msg.text == "mrbangi" then
+	return "hum?!" 
+end
+local function run(msg)
+if msg.text == "hi" then
+	return "hi"
+end
+local function run(msg)
+if msg.text == "bye" then
+	return "bye"
+end
+return {
+	description = "Chat With Robot Server", 
+	usage = "chat with robot",
+	patterns = {
+		"^[Hh]i$",
+		"^[Mm]rbangi$",
+		"^[Bb]angi$",
+		"^[Cc]hannel$",
+		"^[Bb]ye$",
+		}, 
 	run = run,
+    --privileged = true,
+	pre_process = pre_process
 }
