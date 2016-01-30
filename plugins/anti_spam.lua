2249a1d88c058a4d37f3ae6c82bdd003bfc4e83a
mrbangi <xboxlivemnasr@gmail.com>
2016-01-30 16:27:00 +0330

    Update anti_spam.lua

--- plugins/anti_spam.lua
+++ plugins/anti_spam.lua
@@ -1,5 +1,3 @@
-
---An empty table for solving multiple kicking problem(thanks to @topkecleon )
 kicktable = {}
 
 do
@@ -78,6 +76,9 @@ local function pre_process(msg)
         return
       end
       kick_user(user, chat)
+      if msg.chat.type == "user" then
+        block_user("user#id"..msg.from.id,ok_cb,false)--Block user if spammed in private
+      end
       local name = user_print_name(msg.from)
       --save it to log file
       savelog(msg.to.id, name.." ["..msg.from.id.."] spammed and kicked ! ")
@@ -116,7 +117,7 @@ end
 
 local function cron()
   --clear that table on the top of the plugins
-	kicktable = {}
+  kicktable = {}
 end
 
 return {
